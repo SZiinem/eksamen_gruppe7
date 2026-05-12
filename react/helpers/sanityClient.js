850759d781ef605986ea736f9a9381d54d2da86f
filipemamador <filipeamador@outlook.com>
2026-05-12 11:34:19 +0200

    Token

--- react/helpers/sanityClient.js
+++ react/helpers/sanityClient.js
@@ -4,7 +4,7 @@ const client = createClient({
   projectId: "0z7i65ux",
   dataset: "production",
   apiVersion: "2023-10-01",
-  token: "YOUR_API_TOKEN",
+  token: "skIRXAq8DRpnIUSAoLZhgRq1Q9v3xmrPIjBmrcZxnRmdNbTtkO7qPyQQyAeeKIjTIShmRBjTXISGHnLGgf3f77i71mwxUP8XIUq3ALH52CQMVRqPsgNrMtgCAkGFTAccW90DDM4plh3Buz6bkFxzD3rkS38mVmuXw25vBxdIvQRtWf3xsj48",
   useCdn: false
 });
 
