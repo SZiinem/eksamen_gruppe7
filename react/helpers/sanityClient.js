1e6a968468568b34cab0c4e96c82ca6ebd8fefb1
filipemamador <filipeamador@outlook.com>
2026-05-18 13:46:28 +0200

    Sikret apitoken

--- react/helpers/sanityClient.js
+++ react/helpers/sanityClient.js
@@ -4,7 +4,7 @@ const client = createClient({
   projectId: "0z7i65ux",
   dataset: "production",
   apiVersion: "2023-10-01",
-  token: "skIRXAq8DRpnIUSAoLZhgRq1Q9v3xmrPIjBmrcZxnRmdNbTtkO7qPyQQyAeeKIjTIShmRBjTXISGHnLGgf3f77i71mwxUP8XIUq3ALH52CQMVRqPsgNrMtgCAkGFTAccW90DDM4plh3Buz6bkFxzD3rkS38mVmuXw25vBxdIvQRtWf3xsj48",
+  token: import.meta.env.VITE_SANITY_TOKEN, //Lagret token i .env filen for at 
   useCdn: false
 });
 
